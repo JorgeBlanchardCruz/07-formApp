4387494c346862977baf76ee2f45a8cf2d95b92c
Jorge Blanchard Cruz <rothgard@live.com>
2024-05-10 12:18:04 +0100

    next

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ErrorsService } from '../../services/errors.service';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
 })
 export class SwitchesPageComponent {
+
+  public form = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNofitications: [true, Validators.required],
+    termAndConditions: [false, Validators.requiredTrue],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    public errors: ErrorsService
+  ) {
+    this.errors.setForm(this.form);
+  }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.value);
+    this.form.reset();
+  }
 
 }
