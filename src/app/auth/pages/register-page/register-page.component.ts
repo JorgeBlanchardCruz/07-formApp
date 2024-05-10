7dd5a8c82032d374eb363f1b8dee911fc590d8de
Jorge Blanchard Cruz <rothgard@live.com>
2024-05-10 13:14:32 +0100

    next

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorsService } from '../../../reactive/services/errors.service';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public form: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    public errors: ErrorsService
+  ) { }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.value);
+    this.form.reset();
+  }
 
 }
