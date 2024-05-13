162095e4fd19d77d10a2f4dfe5148a2fbd34f7de
Jorge Blanchard Cruz <rothgard@live.com>
2024-05-13 08:52:41 +0100

    next

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidationsService } from '../../../shared/services/validations.service';
+import { EmailValidator } from '../../../shared/services/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,18 +10,26 @@ import { ValidationsService } from '../../../shared/services/validations.service
 export class RegisterPageComponent {
 
   public form: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern(this.validations.firstNameAndLastnamePattern)]],
-    email: ['', [ Validators.required, Validators.pattern(this.validations.emailPattern) ]],
-    username: ['', [ Validators.required, this.validations.cantBeStrider ]],
+    name: ['', [ Validators.required, Validators.pattern(this.validationsService.firstNameAndLastnamePattern)]],
+    email: ['', [ Validators.required, Validators.pattern(this.validationsService.emailPattern)], [this.emailValidator]],
+    username: ['', [ Validators.required, this.validationsService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
-  });
+  }, {
+    validators: [ this.validationsService.IsFieldOneEqualToFieldTwo('password', 'password2') ]
+  }
+  );
+
+  public get customValidators(): ValidationsService {
+    return this.validationsService;
+  }
 
   constructor(
     private fb: FormBuilder,
-    public validations: ValidationsService
+    private validationsService: ValidationsService,
+    private emailValidator: EmailValidator
   ) {
-    this.validations.initialize(this.form);
+    this.validationsService.initialize(this.form);
   }
 
   public onSubmit(): void {
